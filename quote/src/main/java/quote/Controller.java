90c096f15924a6e61d1fa58738838e9e7ee82e0f
Raman Skaskevich <Raman_Skaskevich@epam.com>
2018-02-23 15:57:07 +0100

    Added logging to quote project

--- quote/src/main/java/quote/Controller.java
+++ quote/src/main/java/quote/Controller.java
@@ -4,18 +4,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class Controller {
-	
+	 private static final Logger LOGGER = LoggerFactory.getLogger(Controller.class);
 	@Value("${server.port}")
 	private String applicationPort;
 
 	@RequestMapping(path = "/getQuote", method = RequestMethod.GET)
 	public Quote getQuote() {
+		LOGGER.info("requestType=GET path=getQuote port=" + applicationPort);
 		return new Quote(new Date().toString(), applicationPort);
 	}
 	
